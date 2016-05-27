789bb2d3f89b2359ee71e7d44672315be10b3bb0
yyljj <yyljj@outlook.com>
2016-05-27 18:59:07 +0800

    初始框架

---
+++ src/main/java/com/upcmvc/qilu2016/Qilu2016Application.java
@@ -0,0 +1,12 @@
+package com.upcmvc.qilu2016;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qilu2016Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qilu2016Application.class, args);
+	}
+}
