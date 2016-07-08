0e1dfca4f3fb1288e0ffcd5a504462ae8c8f1ef1
chenzifeng1 <704734862@qq.com>
2016-07-08 21:36:27 +0800

    易班认证的接口

--- src/main/java/com/upcmvc/qilu2016/controller/LoginController.java
+++ src/main/java/com/upcmvc/qilu2016/controller/LoginController.java
@@ -1,8 +1,21 @@
 package com.upcmvc.qilu2016.controller;
 
+import com.upcmvc.qilu2016.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by 陈子枫 on 2016/7/5.
  */
 public class LoginController {
+
+
+    @Autowired
+    LoginService loginService;
+
+    @RequestMapping("/yibanauth")
+    public ModelAndView auth{return loginService.toYibanAuth();}
 
 }
--- src/main/java/com/upcmvc/qilu2016/service/LoginService.java
+++ src/main/java/com/upcmvc/qilu2016/service/LoginService.java
@@ -2,9 +2,12 @@ package com.upcmvc.qilu2016.service;
 
 import com.google.gson.Gson;
 import com.upcmvc.qilu2016.GsonTemplate.VerifyUserInfo;
+import com.upcmvc.qilu2016.util.MCrypt;
+import com.upcmvc.qilu2016.util.SessionUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
@@ -21,6 +24,8 @@ public class LoginService {
     private GetRealMessage getRealMessage;
     @Value("${myconfig.client_id}")
     private static String client_id;
+    @Value("${myconfig.redirect_uri}")
+    private static String redirect_uri;
 
     public boolean procssAuthReal() throws IOException{
         String access_taoken = (String) httpSession.getAttribute("access_token");
@@ -48,5 +53,63 @@ public class LoginService {
             return false;
         }
     }
+
+
+    /**
+     * 判断用户是否携带真实信息(学号)
+     * @return
+     */
+    private boolean isAuthReal(){return httpSession.getAttribute("yi_studentid")!=null;}
+
+    public ModelAndView toYibanAuth(){
+        return new ModelAndView("redirect:https://openapi.yiban.cn/oauth/authorize?client_id=" + client_id + "&redirect_uri=" + redirect_uri);
+    }
+
+    /**
+     * 判断用户是否登陆
+     * @return
+     */
+    public boolean isLogin(){return httpSession.getAttribute("userid")!=null; }
+
+    /**
+     * 处理授权.成功则跳转至/,失败则重新引导用户至易班授权模块
+     * @param verify_request 易班服务器提供的信息,加密的.
+     * @return
+     * @throws Exception
+     */
+    public String processAuth(String verify_request) throws Exception {
+        MCrypt mCrypt = new MCrypt();
+        String res = new String(mCrypt.decrypt(verify_request));
+        if (saveSession(res)) {
+            return "redirect:/";
+        } else {
+            return "redirect:/yibanauth";
+        }
+    }
+
+
+    /**
+     * 完成对解密后的json数据的解析,存在session里.解析失败则是未授权用户,引导其至授权界面.
+     *
+     * @param str
+     */
+    private boolean saveSession(String str) {
+        Gson gson = new Gson();
+        try {
+            SessionUser sessionUser = gson.fromJson(str, SessionUser.class);
+            httpSession.setAttribute("visit_time", sessionUser.visit_time);
+            httpSession.setAttribute("userid", sessionUser.visit_user.userid);
+            httpSession.setAttribute("username", sessionUser.visit_user.username);
+            httpSession.setAttribute("usernick", sessionUser.visit_user.usernick);
+            httpSession.setAttribute("usersex", sessionUser.visit_user.usersex);
+            httpSession.setAttribute("access_token", sessionUser.visit_oauth.access_token);
+            httpSession.setAttribute("token_expires", sessionUser.visit_oauth.token_expires);
+            return true;
+        } catch (Exception ex) {
+            //ex.printStackTrace();
+            return false;
+        }
+    }
+
 }
 
