bc04332a70b394fc201173a9d3ed0d80cc5ac7d2
wanghaojun <549180928@qq.com>
2016-07-15 21:42:33 +0800

    提交正确的小写文件

---
+++ src/main/java/com/upcmvc/qilu2016/config/Config.java
@@ -0,0 +1,32 @@
+package com.upcmvc.qilu2016.config;
+
+/**
+ * Created by wanghaojun on 2016/7/15.
+ */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Jaxlying on 2016/7/12.
+ */
+
+@Component
+public class Config {
+
+    @Value("${qqoauth.appid}")
+    public String appid ;
+
+    @Value("${qqoauth.appkey}")
+    public String appkey;
+
+    @Value("${qqoauth.redirect}")
+    public String redirect_url;
+
+    @Value("${qqoauth.state}")
+    public String state ;
+
+}
