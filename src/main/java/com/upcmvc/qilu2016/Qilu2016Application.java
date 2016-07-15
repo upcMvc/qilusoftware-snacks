65b569662193f10a6e7244450eca5e84fcd7617d
wanghaojun <549180928@qq.com>
2016-07-15 15:20:38 +0800

    增加uploadfilecontroller

--- src/main/java/com/upcmvc/qilu2016/Qilu2016Application.java
+++ src/main/java/com/upcmvc/qilu2016/Qilu2016Application.java
@@ -1,12 +1,31 @@
 package com.upcmvc.qilu2016;
 
+import com.upcmvc.qilu2016.controller.UpLoadFileController;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.FileSystemUtils;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class Qilu2016Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Qilu2016Application.class, args);
+	}
+
+
+
+	@Bean
+	CommandLineRunner init() {
+		return (args) -> {
+			FileSystemUtils.deleteRecursively(new File(UpLoadFileController.ROOT));
+
+			Files.createDirectory(Paths.get(UpLoadFileController.ROOT));
+		};
 	}
 }
